bbac062ddce7106ea49825d632a937bff8153016
Terry Ntow <terry.ntow@gmail.com>
2017-06-08 11:21:02 -0600

    prep repo fo Heroku

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var app = express();
 var path = require('path');
-var port = 3500;
+const PORT = process.env.PORT || 3500;
+
+
+app.use(function(req, res, next){
+    if(req.headers['x-forwarded-proto' === 'http']){
+      next();
+    }else{
+      res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
+
 
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.listen(port, function(){
-  console.log("Server is listening on port " + port);
+app.listen(PORT, function(){
+  console.log("Server is listening on port " + PORT);
 })
