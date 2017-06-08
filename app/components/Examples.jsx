e9b7ef4c9ee50866f1041f25b1e112b3364b5b42
Terry Ntow <terry.ntow@gmail.com>
2017-06-08 12:51:48 -0600

    Added grid system and foundation styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,22 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 //Note that we dont use the props but it has to be there as the only argument
 var Examples = (props) => {
   return(
     <div>
-      <h2>Examples</h2>
-      <p>Welcome to the examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Hawaii'>Hawaii</Link>
+        </li>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
 
   )
